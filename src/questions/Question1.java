a239a8b1a70b0fc7a938d2adce720bba92e22c60
xianzilei <1739575190@qq.com>
2019-09-19 23:13:08 +0800

    question1-两数之和

---
+++ src/questions/Question1.java
@@ -0,0 +1,80 @@
+package questions;
+
+import sun.applet.Main;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * @Auther: xianzilei
+ * @Date: 2019/9/19 22:08
+ * @Description: 两数之和
+ */
+public class Question1 {
+
+    /**
+     * @Description: 解法一：暴力解决
+     * @param nums 给定数组
+     * @param target 目标和
+     * @return: void
+     * @auther: xianzilei
+     * @date: 2019/9/19 22:15
+     **/
+    public static int[] twoSumSolutionOne(int[] nums, int target){
+        //定义初始结果，默认为[-1,-1]
+        int[] result={-1,-1};
+        //从第一个要素循环查找
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                //如果满足结果，则直接结束程序，返回结果
+                if(nums[i]+nums[j]==target){
+                    result[0]=i;
+                    result[1]=j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * @Description: 解法二：两次遍历HashMap
+     * @param nums 给定数组
+     * @param target 目标和
+     * @return: void
+     * @auther: xianzilei
+     * @date: 2019/9/19 22:15
+     **/
+    public static int[] twoSumSolutionTwo(int[] nums, int target){
+        //定义初始结果，默认为[-1,-1]
+        int[] result={-1,-1};
+        //使用hashMap，使用查询元素的复杂度由原来的O(n)降低为近似O(1)
+        HashMap<Integer, Integer> map = new HashMap<>();
+        //先将数据元素入map，其中key为元素值，value为索引位置
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i],i);
+        }
+        //遍历数组元素
+        for (int i = 0; i < nums.length; i++) {
+            //先求出目标和与当前元素的差
+            int targetNum=target-nums[i];
+            //利用差去map中查找是否存在对应的key，如果存在且二者的索引位置不同，则返回结果
+            if(map.containsKey(targetNum)&&i!=map.get(targetNum)){
+                result[0]=i;
+                result[1]=map.get(targetNum);
+                break;
+            }
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums={-2, 7, 11, 15};
+        int target=26;
+        int[] result1 = twoSumSolutionOne(nums, target);
+        int[] result2 = twoSumSolutionOne(nums, target);
+        System.out.println("解法一结果："+ Arrays.toString(result1));
+        System.out.println("解法二结果："+ Arrays.toString(result2));
+    }
+}
