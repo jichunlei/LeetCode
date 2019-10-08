5a184881808d4fc34585461d5a00fe31d01bf75b
xianzilei <1739575190@qq.com>
2019-10-08 08:49:09 +0800

    2、两数相加

---
+++ src/questions/ListNode.java
@@ -0,0 +1,17 @@
+package questions;
+
+/**
+ * @Auther: xianzilei
+ * @Date: 2019/10/7 08:00
+ * @Description: 链表节点对象
+ */
+public class ListNode {
+    //节点值
+    int val;
+    //下一节点引用
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
