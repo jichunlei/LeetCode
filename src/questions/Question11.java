ecafe26936b7cfa4e06c2cd058f222c2aa4f2d00
xianzilei <1739575190@qq.com>
2019-10-11 09:06:27 +0800

    11. 盛最多水的容器 15. 三数之和

---
+++ src/questions/Question11.java
@@ -0,0 +1,58 @@
+package questions;
+
+/**
+ * @Auther: xianzilei
+ * @Date: 2019/10/10 07:58
+ * @Description: 盛最多水的容器
+ */
+public class Question11 {
+
+    /**
+     * 解法一：暴力解决
+     *
+     * @param height
+     * @return: int
+     * @auther: xianzilei
+     * @date: 2019/10/10 8:00
+     **/
+    public static int maxArea1(int[] height) {
+        int result = 0;
+        for (int i = 0; i < height.length-1; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                int temp = Math.min(height[i], height[j]) * (j - i);
+                if (temp > result) {
+                    result = temp;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 解法二：双指针法
+     *
+     * @param height 1
+     * @return: int
+     * @auther: xianzilei
+     * @date: 2019/10/10 8:18
+     **/
+    public static int maxArea2(int[] height) {
+        int i = 0, j = height.length - 1;
+        int result = 0;
+        while (i < j) {
+            if (height[i] < height[j]) {
+                result = Math.max((j - i) * height[i++], result);
+            } else {
+                result = Math.max((j - i) * height[j--], result);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println("解法一结果：" + maxArea1(height));
+        System.out.println("解法二结果：" + maxArea2(height));
+//        System.out.println("解法三结果：" + maxArea1(height));
+    }
+}
