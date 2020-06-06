5856719496e7b199243ed62bb7c925958c3244e6
xianzilei <1739575190@qq.com>
2020-06-06 22:28:53 +0800

    每日一题：2020-06-06

--- src/questions/array/Solution128.java
+++ src/questions/array/Solution128.java
@@ -131,7 +131,7 @@ public class Solution128 {
 
     public static void main(String[] args) {
         int[] nums = {100, 2, 200, 1, 3, 2};
-//        System.out.println(longestConsecutive1(nums));
+        System.out.println(longestConsecutive1(nums));
         System.out.println(longestConsecutive2(nums));
     }
 }
