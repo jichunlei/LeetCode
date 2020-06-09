2f3b4f52f54750d0d84b844ca9f58d0377985e0c
jichunlei <1739575190@qq.com>
2020-06-09 08:32:14 +0800

    最长同值路径 687

--- src/questions/recursion/Solution687.java
+++ src/questions/recursion/Solution687.java
@@ -11,20 +11,42 @@ import pojo.TreeNode;
 public class Solution687 {
     private static int length = 0;
 
+    /**
+     * 递归
+     *
+     * @param root 1
+     * @return int
+     * @author xianzilei
+     * @date 2020/6/9 8:31
+     **/
     public static int longestUnivaluePath(TreeNode root) {
-
-        return 1;
+        arrowLength(root);
+        return length;
     }
 
-//    private static int dfs(TreeNode node) {
-//        if (node == null) {
-//            return 0;
-//        }
-//        int left = dfs(node.left);
-//        int right = dfs(node.right);
-//
-//
-//    }
+    /**
+     * 1、方法的作用：以node为出发点延伸出的最大箭头长度
+     * 2、递归结束条件：node==null，返回0
+     * 3、等价关系式：
+     * 3.1 当node.val=node.left.val时,arrowLength(node)=arrowLength(node.left)+1
+     * 3.2 当node.val=node.right.val,arrowLength(node)=arrowLength(node.right)+1
+     **/
+    private static int arrowLength(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftArrowLength = arrowLength(node.left);
+        int rightArrowLength = arrowLength(node.right);
+        int left = 0, right = 0;
+        if (node.left != null && node.val == node.left.val) {
+            left = leftArrowLength + 1;
+        }
+        if (node.right != null && node.val == node.right.val) {
+            right = rightArrowLength + 1;
+        }
+        length = Math.max(length, left + right);
+        return Math.max(left, right);
+    }
 
     public static void main(String[] args) {
         /**
