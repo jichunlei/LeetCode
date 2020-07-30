c0d010d1fd32b8d803666319dbcb50fbee9d64ee
jichunlei <1739575190@qq.com>
2020-07-30 18:35:14 +0800

    两数之和 II - 输入有序数组

---
+++ src/solutions/twopointers/Solution167.java
@@ -0,0 +1,68 @@
+package solutions.twopointers;
+
+import java.util.Arrays;
+
+/**
+ * 两数之和 II - 输入有序数组
+ *
+ * @author : xianzilei
+ * @date : 2020/7/30
+ */
+public class Solution167 {
+    /**
+     * 解法一：常规思路（双层for循环）
+     *
+     * @param numbers 1
+     * @param target  2
+     * @return int[]
+     * @author xianzilei
+     * @date 2020/7/30 13:29
+     **/
+    public static int[] twoSum1(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    return new int[]{i + 1, j + 1};
+                }
+            }
+        }
+        return new int[]{0, 0};
+    }
+
+    /**
+     * 解法二：双指针法
+     *
+     * @param numbers 1
+     * @param target  2
+     * @return int[]
+     * @author xianzilei
+     * @date 2020/7/30 13:42
+     **/
+    public static int[] twoSum2(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int tmp = numbers[left] + numbers[right];
+            if (tmp == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (tmp > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return new int[]{0, 0};
+    }
+
+    public static int[] twoSum3(int[] numbers, int target) {
+
+        return new int[]{0, 0};
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum1(numbers, target)));
+        System.out.println(Arrays.toString(twoSum2(numbers, target)));
+    }
+}
